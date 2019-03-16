49fc466fb2f490084f19c15756b9274a04d2bb23
wzy <1484818341@qq.com>
2019-03-16 17:17:56 +0800

    complete the interrupt function of fpc1020A

--- NodeMCU/init.lua
+++ NodeMCU/init.lua
@@ -7,13 +7,17 @@ wifi.setmode(wifi.STATION)
 wifi.sta.config{ssid="wzycs7017",pwd="wzyswez7017"}
 led1 = 0    --开门信号灯pin0
 led2 = 4    --拒绝访问信号灯pin4
-button = 3      --开门请求按钮pin3
+collectButton = 3      --采集指纹特征值按钮pin3
+touch = 1          --指纹模块按压信号pin1
+fpcVCC = 2         --指纹模块指纹模组电压pin2
 gpio.write(led1, gpio.HIGH)     --高电平为灯灭 
 gpio.write(led2, gpio.HIGH)
+gpio.write(fpcVCC, gpio.LOW)    --将fpcVCC置低电平
 gpio.mode(led1, gpio.OUTPUT)
 gpio.mode(led2, gpio.OUTPUT)
-gpio.mode(button, gpio.INT, gpio.PULLUP)    --将pin3设为中断模式
-        
+gpio.mode(collectButton, gpio.INT, gpio.PULLUP)    --将pin3设为中断模式
+gpio.mode(touch, gpio.INT)        --将pin1设为中断模式      
+gpio.mode(fpcVCC, gpio.OUTPUT)
 function receiver(socket, string)    --获取TLS服务器返回数据时的回调
     --print(string)
     if (string == '1') then
@@ -30,15 +34,19 @@ end
 tmr.alarm(1, 1000, tmr.ALARM_AUTO, function()   --将nodeMCU连入wifi，并获取IP地址
     if wifi.sta.getip() == nil then
         --print('Waiting for IP ...')
-        uart.write(1, "Waiting for IP ...")     --doesn't work
+        uart.write(1, "Waiting for IP ...")     --doesn't work(可能pin4作为拒绝访问信号灯导致 !todo)
     else
         --print('IP is ' .. wifi.sta.getip())
         uart.write(1, "IP is" .. wifi.sta.getip())      --doesn't work
         sv = tls.createConnection()      --连入网络后，建立一个TLS客户端
         sv:connect(8080, "192.168.1.101")    --连接到用nodejs建立的TLS服务器
-        uart.on("data", 207, function(data)   --注册串口收到数据时的回调
-                sv:send(t.bin2hex(data))               --将串口接收到的数据通过wifi传给服务器
-                --uart.write(0, data)
+        uart.on("data", 215, function(data)   --注册串口收到数据时的回调
+                local collect = gpio.read(collectButton)
+                if collect == 0 then
+                    sv:send("C " .. t.bin2hex(data))               --若采集指纹按钮按下，则视为指纹采集(collect)，将串口接收到的二进制数据转为hex字符串通过wifi传给服务器
+                else
+                    sv:send("R " .. t.bin2hex(data))               --若采集指纹按钮未按下，则视为指纹识别(recognition)，将串口接收到的二进制数据转为hex字符串通过wifi传给服务器
+                end
                 if data == "quit" then
                     uart.on("data")     --unregister callback function
                 end
@@ -51,21 +59,28 @@ tmr.alarm(1, 1000, tmr.ALARM_AUTO, function()   --将nodeMCU连入wifi，并获�
     end
 end)
 
-function ledTrg()   --pin3低电平产生中断时的回调
-    local i = gpio.read(button)
-    local cnt = 0
-    tmr.alarm(1, 1000, tmr.ALARM_AUTO, function()
-        if i == 0 and cnt == 0 then
-            --sv:send('open')
-            uart.write(0, 0xf5, 0x23, 0x00, 0x00, 0x00, 0x00, 0x23, 0xf5)
-            --uart.write(0, t.DATA_START, 0x09, 0x00, 0x00, 0x00, 0x00, t.CmdGenCHK({"09", "00", "00", "00", "00"}), t.DATA_END)
-            cnt = cnt + 1
-        else
-            cnt = 0
-            tmr.stop(1)
-        end
-    end
-    )
+function collectTrg()   --pin3产生下降沿时的回调
+    uart.write(0, 0xf5, 0x23, 0x00, 0x00, 0x00, 0x00, 0x23, 0xf5)       --给指纹模块发送指令，返回指纹特征值
 end
 
-gpio.trig(button, "low", ledTrg)
+function touchTrg()   --当指纹模块受到按压时输出高电平，指纹模块指纹模组供电6s，led2亮6s
+    --local sendTimer = tmr.create()
+    --local cutPowerTimer = tmr.create()
+    gpio.write(led2, gpio.LOW)
+    gpio.write(fpcVCC, gpio.HIGH)
+    tmr.delay(500000)
+    uart.write(0, 0xf5, 0x23, 0x00, 0x00, 0x00, 0x00, 0x23, 0xf5)
+    --tmr.alarm(sendTimer, 500, tmr.ALARM_SINGLE, function()         --延时0.5s后发送返回特征值命令
+        --uart.write(0, 0xf5, 0x23, 0x00, 0x00, 0x00, 0x00, 0x23, 0xf5)       --给指纹模块发送指令，返回指纹特征值
+    --end)
+    --tmr.alarm(cutPowerTimer, 4000, tmr.ALARM_SINGLE, function()         --延时4s后断电
+        --gpio.write(fpcVCC, gpio.LOW)
+        --gpio.write(led2, gpio.HIGH)
+    --end)
+    tmr.delay(4000000)
+    gpio.write(fpcVCC, gpio.LOW)
+    gpio.write(led2, gpio.HIGH)
+end
+
+--gpio.trig(collectButton, "down", collectTrg)     --下降沿触发上传指纹特征值引脚
+gpio.trig(touch, "high", touchTrg)      --上升沿触发对比指纹特征值引脚
--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 1.change tcp-server to tls-server to encrypt the data<br>
 2.add log.txt in NodeJs to record the info that accepted by server<br>
 3.add ADMIN client to manage other clients<br>
-4.repair the problem caused by the connection order of TEST-CLIENT and ADMIN<br>
-5.repair the problem that makes server break down caused by the compulsive shutdown of any connection<br>
+4.fix the problem caused by the connection order of TEST-CLIENT and ADMIN<br>
+5.fix the problem that makes server break down caused by the compulsive shutdown of any connection<br>
 ### 2019-02-15
 1.update openssl<br>
 2.add the ADMIN's authorization<br>
-3.repair the problem that log.txt has been covered caused by the server's breakdown<br>
+3.fix the problem that log.txt has been covered caused by the server's breakdown<br>
 ### 2019-02-16
 1.add ADMIN command password for the request of managing other clients(code maybe confused because of the unknown problem of windows that the Windows PowerShell only can send request once)<br>
 ### 2019-02-17
-1.repair the constant listen of ADMIN password<br>
+1.fix the constant listen of ADMIN password<br>
 2.tidying up code(the problem that ADMIN only can send info once has been solved through adding the request command in the code of ADMIN directly)<br>
 3.merge the sockets array and clientsInfo array<br>
 4.add the judge of ADMIN's command for void client<br>
@@ -22,3 +22,6 @@
 ### 2019-03-11
 1.complete uart to wifi function and tested with 8 byte(F9 00 04 00 00 00 0D F9)<br>
 2.complete the function that extracts characteristic value from finger to server(207 byte)<br> 
+### 2019-03-16
+1.complete the recognition function that uses touching fpc1020A interrupt to wake up the fpc1020A and extract characteristic value to server('R' + 215 byte)<br>
+2.complete the collect function that collects the finger characteristic value by pressing the button(pin3) when recognition('C' + 215 byte)<br>
